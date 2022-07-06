fd0b46367b83e1d71502af7c43582874ef927aad
Jay <johnsonjohnadakole@gmail.com>
2022-07-06 12:32:09 +0100

    new

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className='flex justify-center items-center h-16  bg-black text-white'>
-             <p>Copyright © 2021 Dog All rights reserved.</p>
+             <p>Copyright © 2022 Dog All rights reserved.</p>
         </div>
     )
 }
